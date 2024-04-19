3674f6150dbc8b4d5a49447768417012e81a5c82
Chanel-B <84045976+Chanel-B@users.noreply.github.com>
2024-04-19 15:07:51 +0200

    added basic structure, next we will add the navbar sticky

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// set sticky effect for header
+/*
+//TODO : check me
+window.onscroll = function() { myStickyEffectFunction() };
+
+var navbar = document.getElementById("myTopnav");
+var sticky = navbar.offsetTop;
+
+function myStickyEffectFunction() {
+
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+
+}
+*/
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+
+}
